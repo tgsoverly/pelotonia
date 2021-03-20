c6f986a94761f9e3f92924279f9047e19c8c0b1b
Tim <timothy@overly.me>
2021-03-20 15:36:11 -0400

    Add component

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,28 @@ import Card from '../components/card'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Fund Raising</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <span className="text-green-400"> Pelotonia! </span>
-        </h1>
-        <Card />
-        <p className="mt-3 text-2xl">Start making a difference!</p>
+      <main className="flex flex-col items-center justify-center flex-1 text-center">
+        <div className="px-20">
+          <h1 className="text-6xl font-bold">
+            Welcome to{' '}
+            <span className="text-green-400"> Pelotonia! </span>
+          </h1>
+          <p className="mt-3 text-2xl">Start making a difference!</p>
+        </div>
+        <Card title='Root Caret Pen Holder'
+              body='As part of the Root Peloton, for a small donation, you can receive this thank you gift to hold your favorite writing instrument.'
+              image="pen_holder.jpg"
+              price='25'/>
+        <Card title='Fully Custom Personal Keyboard Case'
+              body='I will work with you personally to construct your dream setup. We will work together to select all your components, materials, and design.'
+              image="keyboard.jpg"
+              price='2000'/>
       </main>
     </div>
   )

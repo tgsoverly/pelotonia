9afbbe47bac0cc59bb18b0197915580ef8695d24
Tim <timothy@overly.me>
2021-03-22 14:50:09 -0400

    Add buttons

--- components/card.js
+++ components/card.js
@@ -1,20 +1,36 @@
-export default function Card({title, body, price, image}) {
-    return (
-      <div className="md:flex bg-gray-100 rounded-xl p-0 my-10 md:my-20">
-        <img className="w-full h-48 md:h-192 md:w-192 object-cover" src={image} alt=""/>
-        <div className="pt-6 p-8 text-center md:text-left">
-            <p className="text-lg font-semibold">
-              {title}
-            </p>
+export default function Card({title, body, price, image, reverse = false}) {
+    const imageElement = (
+            <img className="w-full h-64 md:h-96 md:w-192 object-cover" src={image} alt=""/>
+    );
+    const textAlign = reverse ? "md:text-right" : "md:text-left";
+    const textElement = (
+        <div className={"pt-6 p-8 text-center " + textAlign}>
+          <p className="text-lg font-semibold">
+            {title}
+          </p>
           <div className="font-medium">
-            <div className="text-gray-500">
-              {body}
-            </div>
+            <div className="text-gray-500" dangerouslySetInnerHTML={{__html: body}} />
           </div>
-          <div className="font-semibold">
-            <span>${price}</span>
+          <div className="font-semibold pt-5">
+            <a className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+               href="https://pelotonia.org/donate?recipient=TO0043&recipientType=User"
+               target="_blank">${price}</a>
           </div>
         </div>
-      </div>
     )
+    if(reverse) {
+      return (
+        <div className="md:flex bg-gray-100 p-0 my-5 md:my-10">
+            { textElement }
+            { imageElement }
+        </div>
+      )
+    }else{
+      return (
+        <div className="md:flex bg-gray-100 p-0 my-10 md:my-20">
+            { imageElement }
+            { textElement }
+        </div>
+      )
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,22 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Fund Raising</title>
+        <title>Fund Raising for Pelotonia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 text-center">
         <div className="px-20">
           <h1 className="text-6xl font-bold">
-            Welcome to{' '}
-            <span className="text-green-400"> Pelotonia! </span>
+            Welcome to our{' '}<span className="text-green-400"> Pelotonia </span> Page!{' '}
           </h1>
-          <p className="mt-3 text-2xl">Start making a difference!</p>
+          <p className="mt-3 text-xl">Start making a difference!</p>
         </div>
         <Card title='Root Caret Pen Holder'
-              body='As part of the Root Peloton, for a small donation, you can receive this thank you gift to hold your favorite writing instrument.'
+              body='As part of the <a href="https://pelotonia.org/peloton/a0s3t00000BKXUWAA5">Root Peloton</a>, for a small donation, you can receive this thank you gift to hold your favorite writing instrument.'
               image="pen_holder.jpg"
-              price='25'/>
+              price='25'
+              reverse={true}/>
         <Card title='Fully Custom Personal Keyboard Case'
               body='I will work with you personally to construct your dream setup. We will work together to select all your components, materials, and design.'
               image="keyboard.jpg"
